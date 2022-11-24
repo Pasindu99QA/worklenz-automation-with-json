07aba1c13c195ebc4b1802719788b07011a14abb
Pasindu Ishan <112170265+Pasindu99QA@users.noreply.github.com>
2022-11-24 13:48:08 +0530

    1st commit

--- src/main/java/org/example/Login.java
+++ src/main/java/org/example/Login.java
@@ -29,8 +29,8 @@ public class Login {
 
         driver.manage().window().maximize();
 
-        driver.findElement(By.name("username")).sendKeys("superAdmin");
-        driver.findElement(By.id("password-input")).sendKeys("admin@play#pointz@sh");
+        driver.findElement(By.name("username")).sendKeys("****");
+        driver.findElement(By.id("password-input")).sendKeys("********");
         driver.findElement((By.className("btn"))).click();
 //        Sleeper Thread = null;
         Thread.sleep(5000);
